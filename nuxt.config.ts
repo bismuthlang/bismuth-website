42744a620cb70493565cb8a49f378c8e9a1e5f22
ahf <41027529+ahfriedman@users.noreply.github.com>
2024-08-16 18:31:36 -0500

    updating packages to fix vulns

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
-    app: {
-        head: {
-            link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/logo2.png' }]
-        }
-    },
-    routeRules: {
-        '/api/compile': {
-            cors: true
-        }
-    }
-    // app: {
-    //     head: {
-    //         script: [
-    //             {src: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"},
-    //             {src: "https://cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min.js"}
-    //         ]
-    //     }
-    // }
+  modules: ['@nuxtjs/tailwindcss'],
+
+  app: {
+      head: {
+          link: [
+              { rel: 'icon', type: 'image/x-icon', href: '/logo2.png' }]
+      }
+  },
+
+  // app: {
+  //     head: {
+  //         script: [
+  //             {src: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"},
+  //             {src: "https://cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min.js"}
+  //         ]
+  //     }
+  // }
+  routeRules: {
+      '/api/compile': {
+          cors: true
+      }
+  },
+
+  compatibilityDate: '2024-08-16'
 })
